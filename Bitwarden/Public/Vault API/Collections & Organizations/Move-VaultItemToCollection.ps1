ef42e76af56f36a7327c525ec7887eb897832ef3
John Duprey <jwd@johnwduprey.com>
2022-09-27 18:23:46 -0400

    More cmdlets, cleanup docs

--- Bitwarden/Public/Vault API/Collections & Organizations/Move-VaultItemToCollection.ps1
+++ Bitwarden/Public/Vault API/Collections & Organizations/Move-VaultItemToCollection.ps1
@@ -1,3 +1,50 @@
 function Move-VaultItemToCollection {
+    <#
+    .SYNOPSIS
+    Moves Bitwarden Vault Item to Collection
     
+    .DESCRIPTION
+    Calls /move/{itemid}/{organizationid} 
+    
+    .PARAMETER ItemId
+    Guid of Item
+
+    .PARAMETER OrganizationId
+    Guid of Organization
+
+    .PARAMETER CollectionIds
+    List of CollectionIds
+
+    .LINK
+    https://bitwarden.com/help/vault-management-api/
+    
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]
+        $ItemId,
+        [Parameter(Mandatory = $true)]
+        $OrganizationId,
+        [Parameter(Mandatory = $true)]
+        [string[]]$CollectionIds
+    )
+
+    $Endpoint = 'move/{0}/{1}' -f $ItemId, $OrganizationId
+    $Body = @($CollectionIds) | ConvertTo-Json
+
+    $VaultApi = @{
+        Endpoint = $Endpoint
+        Body     = $Body
+        Method   = 'Post'
+    }
+
+    $Request = Invoke-VaultApi @VaultApi
+
+    if ($Request.success) {
+        $Request.data
+    }
+    else {
+        Write-Host $Request.message
+        $Request.success
+    } 
 }
